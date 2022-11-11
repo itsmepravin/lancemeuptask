7b91ca43b9dc9102237d1e27f64df8d36ad5a1d5
itsmepravin <singh.pravin19@yahoo.com>
2022-11-11 19:57:25 +0545

    Vercel Deploy Issue

--- pages/components/MyStarRating.jsx
+++ pages/components/MyStarRating.jsx
@@ -1,5 +1,4 @@
 const MyStarRating = ({ product }) => {
-  const itemRating = Math.floor(product?.rating?.rate);
   return (
     <div className="position-relative d-flex justify-content-center align-items-center mx-2">
       <div className="d-flex mt-2 gap-2 position-absolute top-0 start-0">
@@ -8,7 +7,7 @@ const MyStarRating = ({ product }) => {
         ))}
       </div>
       <div className="d-flex mt-2 gap-2 position-absolute top-0 start-0">
-        {[...Array(itemRating)].map((_, index) => (
+        {[...Array(3)].map((_, index) => (
           <img key={index} src="/img/goldStar.png" width={13} height={13} />
         ))}
       </div>
